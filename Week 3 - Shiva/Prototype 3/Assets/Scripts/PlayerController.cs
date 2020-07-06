1aa2eaf0f53b1496211877658b67b97778ffb050
pabolusivaram <pabolusivaram@gmail.com>
2020-07-05 22:04:18 -0500

    week3

---
+++ Week 3 - Shiva/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityMod;
+    public bool isOnground = true;
+    public bool gameOver = false;
+    private Animator playerAnimator;
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+    public AudioClip jump;
+    public AudioClip crash;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        Physics.gravity *= gravityMod;
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnground && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnground = false;
+            playerAnimator.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jump, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnground = true;
+            dirt.Play();
+        }
+
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirt.Stop();
+            playerAudio.PlayOneShot(crash, 1.0f);
+        }
+    }
+}
