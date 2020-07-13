afbddf0586bfc21c88248a6f70c0bb4d9765f5c7
pabolusivaram <pabolusivaram@gmail.com>
2020-07-13 14:34:21 -0500

    week5

---
+++ Week 5 /Prototypr 5/Assets/Scripts/Difficultymanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Difficultymanager : MonoBehaviour
+{
+    private Button button;
+    private Gamemanager gameManager;
+    public int difficulty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("GameManager").GetComponent<Gamemanager>();
+        button.onClick.AddListener(setDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
